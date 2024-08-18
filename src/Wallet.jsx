0750f9343505c66f907d386e3e793dcd93c6260c
retr0717 <alwinaji717@gmail.com>
2024-08-18 14:15:32 +0530

    hide create account button

--- src/Wallet.jsx
+++ src/Wallet.jsx
@@ -160,16 +160,17 @@ const Wallet = () => {
           </p>
         </div>
       </div>
-
-      <div className="mb-4">
-        <button
-          onClick={handleCreateAccount}
-          className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Create New Account
-        </button>
-      </div>
-
+      {/* Create New Account Button */}
+      {phrase && keys.privateKey && keys.publicKey && (
+        <div className="mb-4">
+          <button
+            onClick={handleCreateAccount}
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Create New Account
+          </button>
+        </div>
+      )}
       <div className="">
         <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
           ACCOUNTS
